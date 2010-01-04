a78d186bdca13718b9e1295d77c8c9c88139bbb2
Robert Wallhead <rwallhead@gmail.com>
2010-01-04 19:09:01 +1100

    Added a popup for the data and changed the widget to borderless.

--- jecta.py
+++ jecta.py
@@ -1,41 +1,54 @@
 import gtk
 
-class widget:
+
+class appwindow(object):
+    """ This is a window in the application - basic application-wide settings
+        are set here.
+    """
+    window = None
 
     def __init__(self):
-        self.setup()
-        
-    def setup(self):
         self.window = gtk.Window()
+
+
+class labeler(appwindow):
+    pass
+
+
+class dropwidget(appwindow):
+
+    def __init__(self):
+        super(dropwidget, self).__init__()
         w = self.window
-        w.set_size_request(800, 100)
+        w.set_size_request(150, 150)
         w.drag_dest_set(0, [], 0)
+        w.set_opacity(0.75)
+        w.set_keep_above(True)
+        w.set_decorated(False)
+        #w.set_has_frame(True)
         w.connect('drag_motion', self.motion_cb)
         w.connect('drag_drop', self.drop_cb)
         w.connect('drag_data_received', self.got_data_cb)
         w.connect('destroy', lambda w: gtk.main_quit())
-        self.l = gtk.Label()
-        w.add(self.l)
         w.show_all()
         gtk.main()
     def motion_cb(self, wid, context, x, y, time):
-        self.l.set_text('\n'.join([str(t) for t in context.targets]))
+        #self.l.set_text('\n'.join([str(t) for t in context.targets]))
         context.drag_status(gtk.gdk.ACTION_COPY, time)
-        # Returning True which means "I accept this data".
         return True
 
     def drop_cb(self, wid, context, x, y, time):
-        # Some data was dropped, get the data
         wid.drag_get_data(context, context.targets[-1], time)
         return True
 
     def got_data_cb(self, wid, context, x, y, data, info, time):
         # Got data
         text = data.get_text() #gnome files
-        if text is None: # fall-back on win
+        if text is None: # fall-back on windows
             text = data.data[0:-3]
-        self.l.set_text(text)
+        diag = gtk.MessageDialog(message_format=text)
+        diag.show()
         context.finish(True, False, time)
 
 
-widget()
+dropwidget()
