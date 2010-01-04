15fb9af6d2024940a73fb46ace45d8818237540c
Robert Wallhead <rwallhead@gmail.com>
2010-01-04 18:41:23 +1100

    Ugly refactor into a basic widget class
    Very very very WIP...

--- jecta.py
+++ jecta.py
@@ -1,40 +1,41 @@
 import gtk
 
-def motion_cb(wid, context, x, y, time):
-    l.set_text('\n'.join([str(t) for t in context.targets]))
-    context.drag_status(gtk.gdk.ACTION_COPY, time)
-    # Returning True which means "I accept this data".
-    return True
+class widget:
 
-def drop_cb(wid, context, x, y, time):
+    def __init__(self):
+        self.setup()
+        
+    def setup(self):
+        self.window = gtk.Window()
+        w = self.window
+        w.set_size_request(800, 100)
+        w.drag_dest_set(0, [], 0)
+        w.connect('drag_motion', self.motion_cb)
+        w.connect('drag_drop', self.drop_cb)
+        w.connect('drag_data_received', self.got_data_cb)
+        w.connect('destroy', lambda w: gtk.main_quit())
+        self.l = gtk.Label()
+        w.add(self.l)
+        w.show_all()
+        gtk.main()
+    def motion_cb(self, wid, context, x, y, time):
+        self.l.set_text('\n'.join([str(t) for t in context.targets]))
+        context.drag_status(gtk.gdk.ACTION_COPY, time)
+        # Returning True which means "I accept this data".
+        return True
 
-    #import pdb; pdb.set_trace()
+    def drop_cb(self, wid, context, x, y, time):
+        # Some data was dropped, get the data
+        wid.drag_get_data(context, context.targets[-1], time)
+        return True
 
-    # Some data was dropped, get the data
-    wid.drag_get_data(context, context.targets[-1], time)
-    return True
+    def got_data_cb(self, wid, context, x, y, data, info, time):
+        # Got data
+        text = data.get_text() #gnome files
+        if text is None: # fall-back on win
+            text = data.data[0:-3]
+        self.l.set_text(text)
+        context.finish(True, False, time)
 
-def got_data_cb(wid, context, x, y, data, info, time):
 
-    # Got data
-    text = data.get_text() #gnome files
-    if text is None: # fall-back on win
-        text = data.data[0:-3]
-
-#    import pdb; pdb.set_trace()
-
-    l.set_text(text)
-    context.finish(True, False, time)
-
-w = gtk.Window()
-w.set_size_request(800, 100)
-w.drag_dest_set(0, [], 0)
-w.connect('drag_motion', motion_cb)
-w.connect('drag_drop', drop_cb)
-w.connect('drag_data_received', got_data_cb)
-w.connect('destroy', lambda w: gtk.main_quit())
-l = gtk.Label()
-w.add(l)
-w.show_all()
-
-gtk.main()
+widget()
